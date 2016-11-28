1f92ef1da56e0a235c51840c54c2b5b6dcb30101
hiimtaylorjones <taylorjonessoftware@gmail.com>
2016-11-27 19:41:55 -0500

    [models] - creating content and day models

---
+++ app/models/content.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
+
+export default Model.extend({
+  title: attr('string'),
+  body: attr('string'),
+  published: attr('boolean'),
+  day: belongsTo('day')
+});
